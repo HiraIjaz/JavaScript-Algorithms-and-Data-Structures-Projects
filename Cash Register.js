0b6cfbdf5996423807558100be0def94a7beb1db
Hira Ijaz <68460038+HiraIjaz@users.noreply.github.com>
2023-07-13 15:36:36 +0500

    Update Cash Register.js

--- Cash Register.js
+++ Cash Register.js
@@ -45,19 +45,19 @@ function checkCashRegister(price, cash, cid) {
   let cashSum = totalCash(cid);
 
   if (change > cashSum) {
-    return { status: "INSUFFICIENT_FUNDS", change:[] }
+    return { status: 'INSUFFICIENT_FUNDS', change:[] }
   }
   else if (change.toFixed(2) === cashSum) {
-    return { status: "CLOSED", change:cid };
+    return { status: 'CLOSED', change:cid };
   }
 
   let changeInfo = getChange(Curr, cid, change);
   
   if (changeInfo.change > 0){
-    return { status: "INSUFFICIENT_FUNDS", change:[] };
+    return { status: 'INSUFFICIENT_FUNDS', change:[] };
   }
 
-  return { status: "OPEN", change:changeInfo.changeArray };
+  return { status: 'OPEN', change:changeInfo.changeArray };
 }
   
 let result = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
