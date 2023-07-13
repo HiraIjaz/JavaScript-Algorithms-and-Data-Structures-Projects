5ff2ee85d79e40dcbd51ed4bc324554c179776fc
Hira Ijaz <68460038+HiraIjaz@users.noreply.github.com>
2023-07-13 14:54:47 +0500

    Update Caesars Cipher.js

--- Caesars Cipher.js
+++ Caesars Cipher.js
@@ -1,21 +1,24 @@
 function rot13(str) {
   let result = "";
+
   for (let i = 0; i < str.length; i++){
-    if (str[i].match(/[A-Z]/)) {
+    if(str[i].match(/[A-Z]/)){
       let num = str.charCodeAt(i);
-      if (num - 13 < 65) {
+      if (num - 13 < 65){
         num = 90 - ((13 - (num - 65) - 1));
       }
       else {
          num -= 13; 
-        }
+      }
       let c = String.fromCharCode(num);
       result += c;
     }  
     else { 
       result+=str[i]; 
     }
+
   }
+  
   return result;
   }
   
