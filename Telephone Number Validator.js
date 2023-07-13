6b57453e935cd50106652bdc58408a1aa05b0e75
Hira Ijaz <68460038+HiraIjaz@users.noreply.github.com>
2023-07-13 14:55:50 +0500

    Update Telephone Number Validator.js

--- Telephone Number Validator.js
+++ Telephone Number Validator.js
@@ -1,28 +1,31 @@
 function telephoneCheck(str) {
-  if(str[0]==="-"){
-    return false;
-  }
-  if(str.indexOf(" ")==2){
+  if (str[0] === "-") {
     return false;
   }
 
-  if(str.indexOf("(")==-1 && str.indexOf(")")>=0 ){
+  if (str.indexOf(" ") == 2) {
+    return false;
+  } 
+
+  if (str.indexOf("(") == -1 && str.indexOf(")") >= 0 ) {
     return false;
   }
-  if(str.indexOf("(")>=0){
-        if (str[str.indexOf("(")+4]!=")"){
-        return false;
-        }
+
+  if (str.indexOf("(") >= 0) {
+    if (str[str.indexOf("(")+4] != ")") {
+    return false;
     }
+  } 
 
- let phoneNum=str.replace(/-|\s|\(|\)/g, ""); //remove all the spaces and hyphens 
- //console.log(phoneNum)
-  if (phoneNum.length===10){
+ let phoneNum = str.replace(/-|\s|\(|\)/g, ""); //remove all the spaces and hyphens 
+  
+  if (phoneNum.length === 10) {
     return true;
   }
-  else if(phoneNum[0]=="1" && phoneNum.length===11){ //area code should be 1
+  else if (phoneNum[0] == "1" && phoneNum.length === 11) { //area code should be 1
     return true;
   }
+
   return false;
 }
 
