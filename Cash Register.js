f012a30fa348fb69043fefe97b322b206c20b06a
Hira Ijaz <68460038+HiraIjaz@users.noreply.github.com>
2023-07-13 15:18:58 +0500

    Update Cash Register.js

--- Cash Register.js
+++ Cash Register.js
@@ -1,11 +1,11 @@
 function totalCash(cid) {
-  let sum = 0;
+  let cashSum = 0;
 
   for (let i = 0; i < cid.length; i++) {
-    sum += cid[i][1];
+    cashSum += cid[i][1];
   }
 
-  return sum.toFixed(2);
+  return cashSum.toFixed(2);
 }
 
 function getChange(Curr, cid, change) {
@@ -42,12 +42,12 @@ let Curr = {
 
 function checkCashRegister(price, cash, cid) {
   let change = Math.abs(price-cash);
-  let sum = totalCash(cid);
+  let cashSum = totalCash(cid);
 
-  if (change > sum) {
+  if (change > cashSum) {
     return { status: "INSUFFICIENT_FUNDS", change:[] }
   }
-  else if (change.toFixed(2) === sum) {
+  else if (change.toFixed(2) === cashSum) {
     return { status: "CLOSED", change:cid };
   }
 
@@ -60,5 +60,5 @@ function checkCashRegister(price, cash, cid) {
   return { status: "OPEN", change:changeInfo.changeArray };
 }
   
-let r = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-console.log(r);
+let result = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(result);
