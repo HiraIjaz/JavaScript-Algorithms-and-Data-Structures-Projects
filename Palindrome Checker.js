f6877f6883154f1c163f0398c203adcc24129166
Hira Ijaz <68460038+HiraIjaz@users.noreply.github.com>
2023-07-13 14:51:34 +0500

    Update Palindrome Checker.js

--- Palindrome Checker.js
+++ Palindrome Checker.js
@@ -1,7 +1,10 @@
 function palindrome(str) {
   let orignalStr = str.replace(/\s|\W|_/g, "") //removed all the punctuations
+  
   orignalStr = orignalStr.toLowerCase() //converted string to lowercase
+  
   const reversedStr = orignalStr.split("").reverse().join(""); //reversed string
+  
   return orignalStr === reversedStr; //compared orignal string with reversed string
 }
   
