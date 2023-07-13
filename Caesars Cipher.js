34e5bd6c321c0aae21337a57a6060f4d01285e97
Hira Ijaz <68460038+HiraIjaz@users.noreply.github.com>
2023-07-13 14:45:22 +0500

    Update Caesars Cipher.js

--- Caesars Cipher.js
+++ Caesars Cipher.js
@@ -1,24 +1,22 @@
 function rot13(str) {
-    let result=""
-    
-    for (let i=0; i<str.length; i++){
-      if(str[i].match(/[A-Z]/)){
-        let num=str.charCodeAt(i);
-        if(num-13<65){
-          num=90-((13-(num-65)-1));
-        }
-        else num-=13;
-        let c=String.fromCharCode(num);
-        //console.log(c)
-        result+=c;
+  let result = "";
+  for (let i = 0; i < str.length; i++){
+    if (str[i].match(/[A-Z]/)) {
+      let num = str.charCodeAt(i);
+      if (num - 13 < 65) {
+        num = 90 - ((13 - (num - 65) - 1));
       }
       else {
-        result+=str[i];
-      }
-  
+         num -= 13; 
+        }
+      let c = String.fromCharCode(num);
+      result += c;
+    }  
+    else { 
+      result+=str[i]; 
     }
-    //console.log(result)
-    return result;
+  }
+  return result;
   }
   
-  rot13("SERR CVMMN!C");
+rot13("SERR CVMMN!C");
